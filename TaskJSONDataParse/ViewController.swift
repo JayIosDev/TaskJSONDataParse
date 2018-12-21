8c9fcb27f562777537967c74fefc23f538a15e6f
diya kasliwal <kasliwaldiya@gmail.com>
2018-12-21 12:43:07 +0530

    Initial Commit

---
+++ TaskJSONDataParse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskJSONDataParse
+//
+//  Created by Diya Kasliwal on 21/12/18.
+//  Copyright © 2018 Diya Kasliwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
